54b5b3108605af09925e84cf2d5537668e2f0994
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2025-01-06 16:17:59 -0300

    s

--- pages/index.vue
+++ pages/index.vue
@@ -224,7 +224,7 @@ const universidades = [
   {
     carrera: 'Veterinaria',
     universidad: 'U. San Sebastian',
-    ultimo_seleccionado: 0,
+    ultimo_seleccionado: 648,
     ultimo_matriculado: 0,
     ponderaciones: {
       ranking: 40,
